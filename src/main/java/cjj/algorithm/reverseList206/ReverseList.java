400b2440efe1abe865b1eeab30ca4d12997870c4
jiajun <2339300630@qq.com>
2021-09-26 23:44:16 +0800

    便利实现链表反转，并补充代码

--- src/main/java/cjj/algorithm/reverseList206/ReverseList.java
+++ src/main/java/cjj/algorithm/reverseList206/ReverseList.java
@@ -1,13 +1,15 @@
 package cjj.algorithm.reverseList206;
 
-public class Solution {
+public class ReverseList {
     private static ListNode next = null;
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
-        reverseBetween(head,2,4);
+        reverseBetweenWhile(head, 2, 4);
+        //reverseNWhile(head,3);
     }
 
+    // todo 递归实现整个链表反转
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
         if (head.next == null) return head;
@@ -18,7 +20,7 @@ public class Solution {
     }
 
 
-
+    // todo 递归实现部分链表反转
     public static ListNode reverseBetween(ListNode head, int left, int right) {
         if (left == 1) {
             return reverseN(head, right);
@@ -27,6 +29,7 @@ public class Solution {
         return head;
     }
 
+    // todo 递归实现 前n个节点反转
     public static ListNode reverseN(ListNode head, int n) {
         if (n == 1 || head.next == null) {
             next = head.next;
@@ -43,6 +46,52 @@ public class Solution {
         return last;
     }
 
+
+    // todo 便利实现链表反转
+    public static ListNode reverseListWhile(ListNode head) {
+        ListNode p = head;
+        ListNode pre = null;
+        ListNode next = null;
+        while (p != null) {
+            next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
+        }
+        return pre;
+    }
+
+
+    // todo 便利实现前n个节点反转
+    public static ListNode reverseNWhile(ListNode head, int n) {
+        ListNode p = head;
+        ListNode pre = null;
+        ListNode next = null;
+        while (p != null && n > 0) {
+            next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
+            n--;
+        }
+        head.next = next;
+        return pre;
+    }
+
+
+    // todo 便利实现部分链表反转
+    public static ListNode reverseBetweenWhile(ListNode head, int left, int right) {
+        ListNode p = head;
+        if (left == 1) {
+          head = reverseNWhile(head,right);
+        } else {
+            for (int i = 0; i < left - 2; i++) {
+                p = p.next;
+            }
+            p.next = reverseNWhile(p.next, right - left + 1);
+        }
+        return head;
+    }
 
 }
 
